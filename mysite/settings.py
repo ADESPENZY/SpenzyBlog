e178ae883c36593e628ccbaccf6eb2aacb652034
Adespenzy <atoyebijoshua095@gmail.com>
2024-10-17 17:28:25 +0100

    pushing production

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-b2rg*uo13p=tnh1&o_y$i_1-%j0b6y0yfg7z66(ca)(qmiomm^'
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-b2rg*uo13p=tnh1&o_y$i_1-%j0b6y0yfg7z66(ca)(qmiomm^')
+SECRET_KEY = 'django-insecure-b2rg*uo13p=tnh1&o_y$i_1-%j0b6y0yfg7z66(ca)(qmiomm^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True')=="True"
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'spenzyblog.onrender.com']
 
@@ -91,18 +89,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if not DEBUG:
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
